737e5f58cbb93c7a72d722d9e21489a73472d7eb
imsurajx <surajxcode@gmail.com>
2025-11-11 00:23:04 +0530

    SnakeGame : Create Board with Blocks
    Signed-off-by: imsurajx <surajxcode@gmail.com>

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-console.log("JS Connected");
+const board = document.querySelector('.board');
+const blockHeight = 32;
+const blockWidth = 32;
+
+const cols = Math.floor(board.clientWidth / blockWidth);
+const rows = Math.floor(board.clientHeight / blockHeight);
+
+for(let i = 0; i < rows * cols; i++){
+    const block = document.createElement('div');
+    block.classList.add("block");
+    board.appendChild(block);
+}
